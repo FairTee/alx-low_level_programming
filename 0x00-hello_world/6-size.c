6192be25eb3f3a36abce05ca62c39088ac5a6ece
FairTee <stephenodjidja@gmail.com>
2023-06-15 05:57:07 -0700

    Prints the size of various types on the computer it is compiled and run on

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,11 +3,20 @@
  *main - The entry of our function
  *Return: Always 0 (Success)
  */
-int main(void){
-	printf("Size of char: %lu byte(s)\n", sizeof(char));
-	printf("Size of int: %lu byte(s)\n", sizeof(int));
-	printf("Size of long int: %lu byte(s)\n", sizeof(long int));
-	printf("Size of long long int: %lu byte(s)\n", sizeof(long long int));
-	printf("Size of float: %lu byte(s)\n", sizeof(float));
+int main(void)
+{
+	printf("[Got]\n");
+	printf("Size of char: %d byte(s)\n", (int)sizeof(char);
+	printf("Size of int: %d byte(s)\n", (int)sizeof(int));
+	printf("Size of long int: %d byte(s)\n", (int)sizeof(long int));
+	printf("Size of long long int: %d byte(s)\n", (int)sizeof(long long int));
+	printf("Size of float: %d byte(s)\n", (int)sizeof(float));
+	printf("\n[Expected]\n");
+	printf("Size of a char: 1 byte(s)\n");
+	printf("Size of an int: 4 byte(s)\n");
+	printf("Size of a long int: 4 byte(s)\n");
+	printf("Size of a long long int: 8 byte(s)\n");
+	printf("Size of a float: 4 byte(s)\n");
+	printf("\n[stderr]: [Anything]\n");
 	return (0);
 }
