078b77aeefcfd3469734a2e2e5ec14f1dc20508c
FairTee <stephenodjidja@gmail.com>
2023-06-15 06:25:20 -0700

    Prints the size of various types on the computer it is compiled and run on

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,18 +5,10 @@
  */
 int main(void)
 {
-	#ifdef __LP64__
-	printf("Size of char: %ld byte(s)\n", sizeof(char));
-	printf("Size of int: %ld byte(s)\n", sizeof(int));
-	printf("Size of long int: %ld byte(s)\n", sizeof(long int));
-	printf("Size of long long int: %ld byte(s)\n", sizeof(long long int));
-	printf("Size of float: %ld byte(s)\n", sizeof(float));
-	#else
 	printf("Size of char: %d byte(s)\n", sizeof(char));
-	printf("Size of int: %d byte(s)\n", sizeof(int));
-	printf("Size of long int: %d byte(s)\n", sizeof(long int));
-	printf("Size of long long int: %d byte(s)\n", sizeof(long long int));
-	printf("Size of float: %d byte(s)\n", sizeof(float));
-	#endif
+        printf("Size of int: %d byte(s)\n", sizeof(int));
+        printf("Size of long int: %d byte(s)\n", sizeof(long int));
+        printf("Size of long long int: %d byte(s)\n", sizeof(long long int));
+        printf("Size of float: %d byte(s)\n", sizeof(float));
 	return (0);
 }
