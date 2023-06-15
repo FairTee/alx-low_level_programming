99e86961efc5e0855ca4f23c35044f117f578821
FairTee <stephenodjidja@gmail.com>
2023-06-15 04:42:35 -0700

    Prints an exact line

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ *main - The entry of our function
+ *return : Indicates success so prints 0,also checks to error
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
