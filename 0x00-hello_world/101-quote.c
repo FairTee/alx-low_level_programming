d09caa4c1c69570e0dbd011a4113326c94095d57
FairTee <stephenodjidja@gmail.com>
2023-06-15 08:07:51 -0700

    This program retuns 1

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,9 +4,10 @@
  *Return: Always 1 (Failure)
  */
 int main(void)
-{	
+{
 	const char message1[] = "and that piece of art is useful\" - ";
 	const char message2[] = "Dora Korpar, 2015-10-19\n";
+	
 	write(2, message1, sizeof(message1) - 1);
 	write(2, message2, sizeof(message2) - 1);
 	return (1);
