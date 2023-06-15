c432eeb627652b4ba4f21b18d97c731884d6b894
FairTee <stephenodjidja@gmail.com>
2023-06-15 05:02:03 -0700

    Printed using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 /**
  *main - The entry of our function
+ *puts - shows us what we want to see
  *return : Indicates success so prints 0,also checks to error
  */
 int main(void)
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - entry  to our function
+ * return : helps us check for errors
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
