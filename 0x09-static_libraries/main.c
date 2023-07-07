66877f8b8eeec5fcf8120d78099a33040a818b61
FairTee <stephenodjidja@gmail.com>
2023-07-07 13:33:11 -0700

    The static library libmy.a

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
@@ -8,7 +7,6 @@
 
 int main(void)
 {
-	printf("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
-
+	puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
 	return (0);
 }
