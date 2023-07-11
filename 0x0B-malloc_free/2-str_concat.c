6b3862e29b4662b83027c2a817a1e5db96d08b26
FairTee <stephenodjidja@gmail.com>
2023-07-11 01:53:10 -0700

    A function that concatenates two strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,51 +1,45 @@
 #include "main.h"
 #include <stdlib.h>
-
 /**
  * str_concat - get ends of input
  * @s1: input
  * @s2: input
  * Return: 0
  */
-
 char *str_concat(char *s1, char *s2)
 {
-	char *con;
+	char *concat;
 
-	int x, y;
+	int len1 = 0, len2 = 0, x, y;
 
-	if (s1 == NULL)
+	if (s1 == NULL || *s1 == '\0')
 
 		s1 = "";
 
-	if (s2 == NULL)
+	if (s2 == NULL || *s2 == '\0')
 
 		s2 = "";
-	x = y = 0;
 
-	while (s1[x] != '\0')
+	while (s1[len1] != '\0')
 
-		y++;
-	con = malloc(sizeof(char) * (x + y + 1));
+		len1++;
 
-	if (con == NULL)
+	while (s2[len2] != '\0')
+
+		len2++;
+	concat = malloc(sizeof(char) * (len1 + len2 + 1));
+
+	if (concat == NULL)
 
 		return (NULL);
-	x = y = 0;
 
-	while (s1[x] != '\0')
+	for (x = 0; x < len1; x++)
+		concat[x] = s1[x];
 
-	{
-		con[x] = s1[x];
-		x++;
-	}
+	for (y = 0; y < len2; y++)
+		concat[x + y] = s2[y];
 
-	while (s2[y] != '\0')
-	{
-		con[x] = s2[y];
-		x++;
-		y++;
-	}
-	con[x] = '\0';
-	return (con);
+	concat[x + y] = '\0';
+
+	return (concat);
 }
